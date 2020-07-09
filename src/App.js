7261639a0fd1ca52530b1186a47141bdd2bb192e
John Zissimou <ZissimouJ@b2b.regn.net>
2020-07-09 16:30:55 +0100

    Installed jquery. Added css file for App.js. Added a Navbar to the app but it's unfinished.

--- src/App.js
+++ src/App.js
@@ -20,6 +20,26 @@ class App extends React.Component {
 
         return(
             <div className="container">
+                <div className="pos-f-t">
+                    <nav className={`${styles.navbar} navbar`}>
+                        <div className="logo-container">
+                            {/* <img className="logo-img" src={logo} alt="logo" /> */}
+                            <h1>Test</h1>
+                        </div>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                    <div className="collapse" id="navbarToggleExternalContent">
+                        <div className="nav-items-container p-1">
+                            <ul>
+                                <li className="nav-item"><a className="nav-tem-link" href="/">Current Location</a></li>
+                                <li className="nav-item"><a className="nav-tem-link" href="/">Pass Times</a></li>
+                                <li className="nav-item"><a className="nav-tem-link" href="/">People</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <h1>ISS Tracker</h1>
                 <People data={data} />
             </div>
