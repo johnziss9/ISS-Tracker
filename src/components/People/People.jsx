61aecb11b363ad63fb387287582d55dca450da11
John Zissimou <ZissimouJ@b2b.regn.net>
2020-07-09 14:26:52 +0100

    In the people component I've added code to print out images for the number of people. Added the image for the people to the project.

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import person from '../../images/person.png';
 
 const People = ({ data: { number, people }}) => {
+
+    const totalPeople = [];
+
+    for (var i = 1; i <= number; i++)
+        totalPeople.push(i);
+    
+    const peopleInArray = totalPeople.map(() =>
+        <img src={person} alt="person" />
+    );
+
+    console.log(totalPeople);
 
     return (
         <div>
             <h2>People currently on the IIS</h2>
             <p>Number of people currently on the IIS: { number }</p>
+            <div>{peopleInArray}</div>
         </div>
     )
 }
