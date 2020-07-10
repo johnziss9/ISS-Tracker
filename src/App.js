09b0ece32c94c67bba333746acdfa59418676d30
John Zissimou <ZissimouJ@b2b.regn.net>
2020-07-10 10:55:54 +0100

    Added another div in App.js so I can set a background. Added a background image. Changed the globe separator icon.

--- src/App.js
+++ src/App.js
@@ -22,30 +22,32 @@ class App extends React.Component {
         const { data } = this.state;
 
         return(
-            <div className="container">
-                <div className="pos-f-t">
-                    <nav className={`${styles.navbar} navbar`}>
-                        <div className={styles.logoContainer}>
-                            <img className={styles.logo} src={iss} alt="logo" />
-                            <h3 className={styles.title}>ISS Tracker</h3>
-                        </div>
-                        <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className={`${styles.navbarTogglerIcon} navbar-toggler-icon`}></span>
-                        </button>
-                    </nav>
-                    <div className="collapse" id="navbarToggleExternalContent">
-                        <div >
-                            <ul className={styles.navList}>
-                                <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">Current Location</a></li>
-                                <li className={`${styles.navItem} nav-item`}><img src={globe} className={styles.separator} alt="globe" /></li>
-                                <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">Pass Times</a></li>
-                                <li className={`${styles.navItem} nav-item`}><img src={globe} className={styles.separator} alt="globe" /></li>
-                                <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">People</a></li>
-                            </ul>
+            <div className={styles.body}>
+                <div className="container">
+                    <div className="pos-f-t">
+                        <nav className={`${styles.navbar} navbar`}>
+                            <div className={styles.logoContainer}>
+                                <img className={styles.logo} src={iss} alt="logo" />
+                                <h3 className={styles.title}>ISS Tracker</h3>
+                            </div>
+                            <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className={`${styles.navbarTogglerIcon} navbar-toggler-icon`}></span>
+                            </button>
+                        </nav>
+                        <div className="collapse" id="navbarToggleExternalContent">
+                            <div >
+                                <ul className={styles.navList}>
+                                    <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">Current Location</a></li>
+                                    <li className={`${styles.navItem} nav-item`}><img src={globe} className={styles.separator} alt="globe" /></li>
+                                    <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">Pass Times</a></li>
+                                    <li className={`${styles.navItem} nav-item`}><img src={globe} className={styles.separator} alt="globe" /></li>
+                                    <li className={`${styles.navItem} nav-item`}><a className={`${styles.navItemLink} nav-tem-link`} href="/">People</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                    <People data={data} />
                 </div>
-                <People data={data} />
             </div>
         )
     }
