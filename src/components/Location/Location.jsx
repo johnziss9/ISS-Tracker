2f62f36134cdd4a89767c9eedeb989d106ba1897
John Z <johnziss9@gmail.com>
2020-07-11 15:29:20 +0100

    Installed google-maps-react. Changed the Location component into a class. Added map into the component. Styled map. Added dummy latitude and longitude. Added icon that will replace the google maps marker.

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import styles from './Location.module.css';
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
+// import mapIcon from '../../images/iss_map_icon.png';
 
-const Location = () => {
+class Location extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            name: "React"
+        };
+    }
 
-    return (
-        <div className={`${styles.container} container`}>
-            <h2>Current Location of ISS</h2>
-        </div>
-    )
+    render() {
+
+        const mapStyles = {
+            width: '100%',
+            height: '100%'
+        };
+
+        const containerStyle = {
+            width: '50%',
+            height: '50%'
+          }
+
+        return (
+
+            <div className={`${styles.container} container`}>
+                <h2>Current Location of ISS</h2>
+                <Map
+                    google={this.props.google}
+                    zoom={3}
+                    style={mapStyles}
+                    containerStyle={containerStyle}
+                    className={styles.map}
+                    initialCenter={{
+                        lat: 40.5230,
+                        lng: -99.8445
+                    }}
+                    width="50%"
+                    height="50%"
+                >
+                   <Marker
+                        onClick={this.onMarkerClick}
+                        name={'ISS Location'}
+                        // icon={{
+                        //     url: '../../images/iss_map_icon.png',
+                        //     anchor: new google.maps.Point(32,32),
+                        //     scaledSize: new google.maps.Size(64,64)
+                        // }}
+                    />
+                </Map>
+            </div>
+        )
+    }
 }
 
-export default Location;
+export default GoogleApiWrapper ({
+    apiKey: 'AIzaSyAm3UWmkAA37FFrVCi16-r5Bc4TMpgG1VA'
+})(Location);
