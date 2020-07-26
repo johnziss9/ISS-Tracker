73a539d180524783e529c927f16b7a964b57e241
John Zissimou <ZissimouJ@b2b.regn.net>
2020-07-26 22:48:06 +0100

    Converted PassTimes data to readable format. Fixed issue with white background if height was shorter than content.

--- src/components/PassTimes/PassTimes.jsx
+++ src/components/PassTimes/PassTimes.jsx
@@ -13,15 +13,43 @@ class PassTimes extends React.Component {
             items.push(response[item]);
         }
 
+        function getTime(timeItem) {
+            const fetchedTime = timeItem;
+
+            const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+            const date = new Date(fetchedTime * 1000);
+            const year = date.getFullYear();
+            const month = months[date.getMonth()];
+            const day = date.getDate();
+            const hours = date.getHours();
+            const minutes = "0" + date.getMinutes();
+            const seconds = "0" + date.getSeconds();
+
+            const converted = day + '/' + month + '/' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2)
+
+            return converted;
+        }
+
+        function getDuration(durationItem) {
+
+            const minutes = Math.floor(durationItem / 60);
+            const seconds = durationItem - minutes * 60;
+
+            const duration = minutes + ' minutes and ' + seconds + ' seconds';
+
+            return duration;
+        }
+
         return (
 
             <div className={`${styles.container} container`}>
                 <h2>Next Preticted Pass Times of ISS on Current Location</h2>
                 <h4>Time & Duration</h4>
                 {items.map((item, i) =>
-                <h2 key={i}>
-                    {item.risetime} - {item.duration}
-                </h2>)}
+                <h6 key={i}>
+                    {getTime(item.risetime)} - {getDuration(item.duration)}
+                </h6>)}
             </div>
         )
     }
